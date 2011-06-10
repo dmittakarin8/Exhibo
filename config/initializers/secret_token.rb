ca39ffc50a030146feff3123f6552e24082d9846
Denis Mittakarin <dmittakarin8@hotmail.com>
2011-06-10 13:05:29 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exhibo::Application.config.secret_token = '3133576f32a7a0cca1d249d9f13864c7b9c4a016aa7a0ddb03e82bc5979dcc0d58c716f11827737fd6c68f056dde1b47d443fbc12aa7240e8d828e1a2778a3a4'
